796990bc154ad3ca4694483d7e12c18cab0daa42
SaquiNda <eliasdavidsch@hotmail.com>
2023-05-17 22:03:53 -0500

    Prueba

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule
+    //PageTransitionEvent
   ],
   providers: [],
   bootstrap: [AppComponent]
